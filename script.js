5521b43f790ea5d321e0bf47afbc5367a7ae5970
Dipanshi Verma <dipanshiverma0@gmail.com>
2025-06-18 17:47:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
-    //Write your code here
+  const form = document.getElementById("form1");
+  const firstName = form.fname.value.trim();
+  const lastName = form.lname.value.trim();
 
+  alert(`${firstName} ${lastName}`);
+  
+  // Prevent default form submission
+  return false;
 }
